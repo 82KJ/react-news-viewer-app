fb0f5a302fd412f953eb04c31599245d05de564c
KwanJung98 <remf123@gmail.com>
2022-02-26 17:43:25 +0900

    News API 호출로 Data 연동

--- src/App.js
+++ src/App.js
@@ -3,18 +3,6 @@ import axios from 'axios';
 import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${process.env.REACT_APP_API_KEY}`,
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
   return <NewsList />;
 };
 
